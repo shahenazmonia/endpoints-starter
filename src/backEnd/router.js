f98df28756abf4c943105de33930fdd965a9a5d3
shahenazmonia <shahy.m.93@gmail.com>
2018-09-06 12:28:57 +0300

    create router file and call functions and validation

--- src/backEnd/router.js
+++ src/backEnd/router.js
@@ -1,1 +1,8 @@
+import express from 'express';
+import {celebrate} from 'celebrate';
+import {signupValidation} from './middleware/validation';
+import {signup} from './Controllers/signup';
+const router= express.Router();
 
+module.exports = router
+  .post('/api/signup', celebrate(signupValidation), signup);
