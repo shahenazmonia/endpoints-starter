2e750b770f19bf7d6aec806b0daabfc93544c981
shahenazmonia <shahy.m.93@gmail.com>
2018-09-06 12:30:56 +0300

    make validation for signup fun

---
+++ src/backEnd/middleware/validation/user.js
@@ -0,0 +1,12 @@
+import Joi from 'joi';
+
+export const signup = {
+  body:{
+    username: Joi.string().required(),
+    password: Joi.string().required(),
+    confirmPassword: Joi.string().required()
+      .valid(Joi.ref('password')).options({
+        language: {any: {allowOnly: '!!Passwords do not match'} }
+      })
+  }
+};
