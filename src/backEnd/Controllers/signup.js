9805568ef2ec43a85c905cc0510d0c8cbd626d92
shahenazmonia <shahy.m.93@gmail.com>
2018-09-06 12:27:48 +0300

    create signup function

---
+++ src/backEnd/Controllers/signup.js
@@ -0,0 +1,32 @@
+import {Sequelize, Users} from '../../database/models';
+import hashPassword from '../utils/hashPassword';
+export const signup = (req, res) =>{
+  const {body: {username, password} } = req;
+
+  Users.findOne({where: {username} })
+    .then((result)=>{
+      if(result)
+        return res.status(200).json({msg: 'the user already exists'});
+      return Sequelize.transaction((t)=>{
+        return hashPassword(password)
+          .then((hashedPassword)=>{
+            return  Users.create({
+              username,
+              password: hashedPassword,
+            }, {transaction: t})
+              .then((user)=>{
+                delete user.dataValues.password;
+                return res.status(200).json(user);
+              })
+              .catch(()=>{
+                t.rollback();
+              });
+          },{transaction: t})
+          .catch(()=>{
+            t.rollback();
+          });
+      }).catch(()=>{
+        return res.status(500).json({err: 'error in query'});
+      });
+    });
+};
