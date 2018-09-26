70b2923dc2cc22acc9fd576c2d38b6a2fffb0018
shahenazmonia <shahy.m.93@gmail.com>
2018-09-26 13:56:19 +0300

    add hash router to app

--- src/frontEnd/components/app.js
+++ src/frontEnd/components/app.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import {HashRouter, Route, Switch, Redirect} from 'react-router-dom';
 import Signup from './containers/signup';
 import Login from './containers/login';
+const publicPath = '/';
+
+export const routeCodes = {
+  SINGUP: `${publicPath}signup`,
+  LOGIN: `${publicPath}login`
+};
+
 const App = () => {
 
   return(
-    <Login> </Login>
-    // <Signup> </Signup>
+    <HashRouter>
+      <Switch>
+        <Route exact path={publicPath} component={Login}/>
+        <Route exact path={routeCodes.SINGUP} component={Signup}/>
+        <Route exact path={routeCodes.LOGIN} component={Login}/>
+        <Redirect to ='/' />
+      </Switch>
+    </HashRouter>
   );
 };
 
